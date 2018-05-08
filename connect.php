5d766f9fdd9b7c2e8ed24fbaccedb53e32f9dc49
Chotiwut Sattayapisan <ezio.magtap@gmail.com>
2018-05-08 21:29:35 +0700

    test1

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = new mysqli('localhost','root','','senior_project');
+$db = new mysqli('localhost','root','','siitvanm_Senior');
 $db->query('SET NAMES UTF8');
 
    if($db->connect_errno){
